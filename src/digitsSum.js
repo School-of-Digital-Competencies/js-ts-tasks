b4b1f969a5248777a4046fbedf210d51db582744
Lest38 <132215478+Lest38@users.noreply.github.com>
2024-10-25 12:04:27 +0300

    Update digitsSum.js

--- src/digitsSum.js
+++ src/digitsSum.js
@@ -4,5 +4,10 @@
  * @returns {number}
  */
 module.exports.digitsSum = function digitsSum(n) {
-  throw new Error('Not implemented'); // remove this line
+  let sum = 0;
+  for (let i = 0; i < n; i++)
+  {
+    sum += i;
+  }
+  return sum;
 };
