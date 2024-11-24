bf03e9b4739279b264f8d2dc4b5df68fe4e91439
Yury Stukau <youremailaddress@student.ehu.lt>
2024-11-24 18:46:28 +0300

    feat: updated maxCommonSub implementation

--- src/maxCommonSub.js
+++ src/maxCommonSub.js
@@ -9,15 +9,15 @@
  * @returns {number}
  */
 module.exports.maxCommonSub = function maxCommonSub(str1, str2) {
-  let maxSub = ''; // Переменная для хранения максимальной подстроки
-
-  for (let i = 0; i < str1.length; i++) {
-    for (let j = i + 1; j <= str1.length; j++) {
-      const substring = str1.slice(i, j); // Получаем подстроку из str1
-      if (str2.includes(substring) && substring.length > maxSub.length) {
-        maxSub = substring; // Если подстрока есть в str2 и она длиннее maxSub, обновляем maxSub
-      }
+  // тут честно пользовался помощником
+  let maxSub = ''; // переменную для хранения самой длинной общей подстроки
+  for (let i = 0; i < str1.length; i++) { // первый цикл перебирает все возможные начальные позиции подстрок в строке str1
+  for (let j = i + 1; j <= str1.length; j++) { // второй цикл перебирает все возможные конечные позиции подстрок в строке str1
+    const substring = str1.slice(i, j); // slice, чтобы получить подстроку из строки str1, где Подстрока начинается с позиции i и заканчивается перед позицией j
+    if (str2.includes(substring) && substring.length > maxSub.length) { // если оба условия выполнены, обновляем maxSub
+    maxSub = substring; 
     }
+  }
   }
 
   return maxSub;
