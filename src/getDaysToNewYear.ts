5ec30cd0447cc281f6bad9d94e1810f85df00bf7
Ivan Lemesh <vanya_lemesh@outlook.com>
2025-02-01 15:08:34 +0200

    fix

--- src/getDaysToNewYear.ts
+++
@@ -1,7 +0,0 @@
-module.exports.getDaysToNewYear = function getDaysToNewYear(targetDate: Date | string): number {
-    const date = new Date(targetDate);
-    const nextNewYear = new Date(date.getFullYear() + 1, 0, 1);
-    const diffTime = nextNewYear.getTime() - date.getTime();
-    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-    return diffDays;
-};
