93b1dd7b200f1792dcc3b931b2f0d6fd77aa5a3c
Artem <artem.chernihivskyi@stud.esdc.lt>
2025-11-03 23:00:22 +0200

    homework done

--- src/backToFront.js
+++ src/backToFront.js
@@ -1,9 +1,20 @@
 /**
- * Copy and paste defined amount of a string's symbols to the front and back of a string
+ * Copy and paste defined amount of a string's symbols
+ * to the front and back of a string
  * @param {string} str
  * @param {number} symbolsCount
  * @returns {string}
  */
 module.exports.backToFront = function backToFront(str, symbolsCount) {
-  throw new Error('Not implemented');
+  if (str.length === 0) {
+    return '';
+  }
+
+  if (symbolsCount === 0 || symbolsCount > str.length) {
+    return str;
+  }
+
+  const back = str.slice(-symbolsCount);
+
+  return `${back}${str}${back}`;
 };
