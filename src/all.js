a55773d306651d6666d1e7573af1bb4f3b69cba0
greff379 <igor.greff@yandex.ru>
2026-04-02 13:09:56 +0300

    feat: homework 6 hryhoryeu ihar

--- src/all.js
+++ src/all.js
@@ -3,6 +3,29 @@
  * @param {Array<Promise>} promisesArray
  * @returns Promise
  */
+
 module.exports.all = function all(promisesArray) {
-  throw new Error('Not implemented'); // remove me and write your code
+  return new Promise((resolve, reject) => {
+    if (promisesArray.length === 0) {
+      resolve([]);
+      return;
+    }
+
+    const results = [];
+    let completedCount = 0;
+    promisesArray.forEach((promise, index) => {
+      Promise.resolve(promise)
+        .then(value => {
+          results[index] = value;
+          completedCount++;
+
+          if (completedCount === promisesArray.length) {
+            resolve(results);
+          }
+        })
+        .catch(error => {
+          reject(error);
+        });
+    });
+  });
 };
